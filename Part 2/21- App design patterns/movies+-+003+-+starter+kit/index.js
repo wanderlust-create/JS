309a28c16ef278b6556de031f30f1091e40ed76a
Tamara Dowis <tamara.dowis@gmail.com>
2022-09-13 20:42:59 -0500

    update movie API call structure

--- Part 2/21- App design patterns/movies+-+003+-+starter+kit/index.js
+++ Part 2/21- App design patterns/movies+-+003+-+starter+kit/index.js
@@ -5,18 +5,89 @@ const fetchData = async (searchTerm) => {
             s: searchTerm
         }
     })
+    // check for status 200 but no results
+    // better: return error message
+    if (response.data.Error) {
+        return []
+    }
     return response.data.Search
 }
-
+const root = document.querySelector('.autocomplete')
+// creating this in the JS file allows duplication of the autocomplete and decoupling of the dependent code in HTML
+root.innerHTML = `
+    <label><b>Search for a movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`
 const input = document.querySelector('input')
+const dropdown = document.querySelector('.dropdown')
+const resultsWrapper = document.querySelector('.results')
 
 const onInput = async event => {
     // will return a promise because fetchData is an asyn func
     const movies = await fetchData(event.target.value)
-    
+    // with no results, better to add message
+    if (!movies.length) {
+        dropdown.classList.remove('is-active')
+        return
+    }
+    resultsWrapper.innerHTML = '' // clear existing when new search is made 
+    dropdown.classList.add('is-active')// Bluma
     for (let movie of movies) {
-        
+        const movieOption = document.createElement('a')// 'a' from Bluma requirement
+        const imgSRC = movie.Poster === 'N/A' ? '' : movie.Poster // if link is missing
+
+        movieOption.classList.add('dropdown-item')// Bluma class
+        movieOption.innerHTML = `
+        <img src="${imgSRC}">
+        ${movie.Title}
+        `
+        movieOption.addEventListener('click', () => {
+            dropdown.classList.remove('is-active')
+            input.value = movie.Title
+            onMovieSelect(movie)
+        })
+        resultsWrapper.appendChild(movieOption)
     }
 }
 input.addEventListener('input', debounce(onInput, 500))
-  
+// to close the dropdown menu if user clicks outside of the root
+document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active')
+    }
+})
+
+const onMovieSelect = async movie => {
+        const response = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: "53fdb133",
+            i: movie.imdbID
+        }
+        document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+    })
+
+    const movieTemplate = (movieDetail) => {
+        return `
+            <artile class="media">
+                <figure class-"media-left>
+                    <p class="image">
+                        <img src="${movieDetail.Poster}" />
+                    </p>
+                </figure>
+                <div class="media-content">
+                    <div class="content">
+                        <h1>${movieDetail.Title}</h1>
+                        <h4>${movieDetail.Genre}</h4>
+                        <p>${movieDetail.plot}</p>
+                    </div>
+                </div>
+            </article>
+
+        `
+    }
+}
